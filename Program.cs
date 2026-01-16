42d10e566a638838dd8471c4bce7bed0718f2e83
Hancer <hancer.mercede@arroyoconsulting.net>
2026-01-16 15:14:39 -0400

    adding the xml comments to the Either implementation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using Either_Implementation.Either;
-
+Console.WriteLine("This is an example with Either.");
 var example = new Example();
 await example.ExampleWithEither();
 Console.ReadKey();
+
+
